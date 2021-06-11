bbef2b6fccfb2a4e3786cbc9a56150fb25320a9a
unknown <austinhansen97@gmail.com>
2021-06-11 16:51:35 -0600

    I am on the last task (task 7). I have 1 failed left

--- index.js
+++ index.js
@@ -264,9 +264,12 @@ get20s(artists);
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, index) {
+  array.splice(index, 1);
+  return array.length;
 }
+
+removeArtist(artists, 9);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
@@ -284,9 +287,22 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array, newObject) {
+  array.push(newObject);
+  console.log(array);
+  return array;
 }
+
+const stuff = {
+  id: 20,
+  name: "Austin Hansen",
+  years: "1997 - 2021",
+  genre: "Web Design",
+  nationality: "USA",
+  bio: "I am an artist.",
+};
+
+addArtist(artists, stuff);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use lotsOfArt to do the following: 
